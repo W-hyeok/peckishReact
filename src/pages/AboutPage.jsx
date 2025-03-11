0149e8cfa1cb757a8b2b5e80b68ad25d135fb9d4
HongSaek-Ko <bh1152@naver.com>
2025-03-12 00:02:00 +0900

    소개 페이지 css 조정

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,19 +1,87 @@
 import useCustomLogin from '../hooks/useCustomLogin';
 import BasicLayout from '../layouts/BasicLayout';
 ('use client');
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Popover,
+  PopoverBackdrop,
+  PopoverButton,
+  PopoverPanel,
+} from '@headlessui/react';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Dialog, DialogPanel } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import useCustomMove from '../hooks/useCustomMove';
+import { getCookie } from '../util/cookieUtil';
+import { API_SERVER_HOST } from '../api/todoApi';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import fishLogo from '/src/assets/fish_logo.png';
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
-];
+// const navigation = [
+//   { name: 'Product', href: '#' },
+//   { name: 'Features', href: '#' },
+//   { name: 'Marketplace', href: '#' },
+//   { name: 'Company', href: '#' },
+// ];
+
+const host = API_SERVER_HOST;
 
 const AboutPage = () => {
+  const loginState = useSelector((state) => state.loginSlice);
+  useEffect(() => {
+    console.log(loginState.roleNames);
+  }, []);
+
+  let numStat = 0;
+
+  if (loginState.email) {
+    numStat = loginState.roleNames.length;
+  } else {
+    numStat = 0;
+  }
+  let cookieMember = getCookie('member')
+    ? getCookie('member')
+    : { email: null };
+
+  useEffect(() => {
+    console.log('cookie 확인 *****: {}', cookieMember);
+  }, []);
+  // 로그인 이전 볼 수 있는 링크
+  const userNavigation = [
+    { name: '로그인', href: '/member/login' },
+    { name: '회원가입', href: '/member/add' },
+  ];
+
+  // 로그인한 일반 사용자만 볼 수 있는 링크
+  const userNavigationUser = [
+    { name: '내 프로필 보기', href: `/member/read/${cookieMember.email}` },
+    { name: '로그아웃', href: '/member/logout' },
+    { name: '회원탈퇴', href: '/member/leave' },
+  ];
+
+  // 로그인한 사업자만 볼 수 있는 링크
+  const userNavigationOwner = [
+    { name: '내 프로필 보기', href: `/member/read/${cookieMember.email}` },
+    { name: '점포관리', href: `/shop/detailMoon/${cookieMember.email}` },
+    { name: '영업 시작/종료', href: `/member/openClose/${cookieMember.email}` },
+    { name: '로그아웃', href: '/member/logout' },
+    { name: '회원탈퇴', href: '/member/leave' },
+  ];
+
+  // 로그인한 관리자만 볼 수 있는 링크
+  const userNavigationAdmin = [
+    { name: '전체 회원 목록', href: '/admin/list' },
+    { name: '전체 점포 목록', href: '/admin/adminshop/shoplist' },
+    { name: '전체 사업자 신청 목록', href: '/admin/adminmember/memberlist' },
+    { name: '로그아웃', href: '/member/logout' },
+  ];
+
+  const { moveToLogin, moveToMain } = useCustomMove();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
@@ -26,11 +94,7 @@ const AboutPage = () => {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
+              <img alt="" src={fishLogo} className="h-12 w-auto" />
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -40,7 +104,8 @@ const AboutPage = () => {
               className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon aria-hidden="true" className="size-6" />
+              {/* <Bars3Icon aria-hidden="true" className="size-6" /> */}
+              {/* Profile dropdown */}
             </button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
@@ -54,13 +119,76 @@ const AboutPage = () => {
               </a>
             ))} */}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm/6 font-semibold text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+          <div className="lg:flex lg:flex-1 lg:justify-end">
+            <Menu as="div" className="relative ml-4 shrink-0">
+              <div>
+                <MenuButton className="relative flex rounded-full bg-white text-sm ring-2 ring-white/20 focus:outline-none focus:ring-white">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  {cookieMember ? (
+                    <img
+                      alt=""
+                      src={`${host}/api/member/view/${cookieMember.profileFilename}`}
+                      className="size-12 rounded-full"
+                    />
+                  ) : (
+                    <></>
+                  )}
+                </MenuButton>
+              </div>
+              <MenuItems
+                transition
+                className="absolute -right-2 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-none data-[closed]:data-[leave]:scale-95 data-[closed]:data-[leave]:transform data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-75 data-[leave]:ease-in"
+              >
+                {/* 로그인한 사용자만 출력되는 메뉴 (웹 크기) */}
+                {loginState.email
+                  ? authCookie
+                    ? userNavigationAdmin.map((item) => (
+                        <MenuItem key={item.name}>
+                          <Link
+                            to={item.href}
+                            className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                          >
+                            {item.name}
+                          </Link>
+                        </MenuItem>
+                      ))
+                    : numStat == 2
+                      ? userNavigationOwner.map((item) => (
+                          <MenuItem key={item.name}>
+                            <Link
+                              to={item.href}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                            >
+                              {item.name}
+                            </Link>
+                          </MenuItem>
+                        ))
+                      : userNavigationUser.map((item) => (
+                          <MenuItem key={item.name}>
+                            <Link
+                              to={item.href}
+                              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                            >
+                              {item.name}
+                            </Link>
+                          </MenuItem>
+                        ))
+                  : userNavigation.map((item) => (
+                      <MenuItem key={item.name}>
+                        <Link
+                          to={item.href}
+                          className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
+                        >
+                          {item.name}
+                        </Link>
+                      </MenuItem>
+                    ))}
+              </MenuItems>
+            </Menu>
           </div>
         </nav>
-        <Dialog
+        {/* <Dialog
           open={mobileMenuOpen}
           onClose={setMobileMenuOpen}
           className="lg:hidden"
@@ -88,7 +216,7 @@ const AboutPage = () => {
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
-                  {/* {navigation.map((item) => (
+                  {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
@@ -96,11 +224,11 @@ const AboutPage = () => {
                     >
                       {item.name}
                     </a>
-                  ))} */}
+                  ))}
                 </div>
                 <div className="py-6">
                   <a
-                    href="#"
+                    onClick={moveToLogin}
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     Log in
@@ -109,7 +237,7 @@ const AboutPage = () => {
               </div>
             </div>
           </DialogPanel>
-        </Dialog>
+        </Dialog> */}
       </header>
 
       <div className="relative isolate pt-14">
@@ -137,14 +265,14 @@ const AboutPage = () => {
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={moveToMain}
+                  className="rounded-md bg-yellow-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
                 >
-                  Get started
+                  시작하기
                 </a>
-                <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                {/* <a href="#" className="text-sm/6 font-semibold text-gray-900">
                   Learn more <span aria-hidden="true">→</span>
-                </a>
+                </a> */}
               </div>
             </div>
             {/* 캐러샐 부분? */}
